09a848acde8a9d748ee4cbd142b85b1d7bac5abd
Joe Krall <hello@joekrall.com>
2024-04-02 19:34:03 -0700

    wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,19 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.window_background_opacity = 0.93
+
+config.font_size = 18
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+
+-- and finally, return the configuration to wezterm
+return config
